d82f9525f6643465b37163f7a5ac24bce1a4e9fc
Sarimoca <sarimoca@gmail.com>
2025-03-26 12:05:53 -0600

    Implementación de la intefaz

--- MessageQueueSystem/TestApplication/Form1.Designer.cs
+++ MessageQueueSystem/TestApplication/Form1.Designer.cs
@@ -28,12 +28,341 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            listView1 = new ListView();
+            button2 = new Button();
+            button3 = new Button();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            groupBox3 = new GroupBox();
+            comboBox1 = new ComboBox();
+            richTextBox1 = new RichTextBox();
+            button4 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox4 = new GroupBox();
+            comboBox2 = new ComboBox();
+            label8 = new Label();
+            label9 = new Label();
+            button5 = new Button();
+            listView2 = new ListView();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            groupBox5 = new GroupBox();
+            richTextBox2 = new RichTextBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(groupBox1, "groupBox1");
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Name = "groupBox1";
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(textBox2, "textBox2");
+            textBox2.Name = "textBox2";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.BackColor = SystemColors.HotTrack;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(textBox3, "textBox3");
+            textBox3.Name = "textBox3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.Name = "label4";
+            label4.Click += label4_Click;
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(groupBox2, "groupBox2");
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(listView1);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.TabStop = false;
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(textBox4, "textBox4");
+            textBox4.Name = "textBox4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(label5, "label5");
+            label5.Name = "label5";
+            // 
+            // listView1
+            // 
+            resources.ApplyResources(listView1, "listView1");
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2 });
+            listView1.Name = "listView1";
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(button2, "button2");
+            button2.BackColor = SystemColors.HotTrack;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(button3, "button3");
+            button3.BackColor = SystemColors.HotTrack;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Name = "button3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // columnHeader1
+            // 
+            resources.ApplyResources(columnHeader1, "columnHeader1");
+            // 
+            // columnHeader2
+            // 
+            resources.ApplyResources(columnHeader2, "columnHeader2");
+            // 
+            // groupBox3
+            // 
+            resources.ApplyResources(groupBox3, "groupBox3");
+            groupBox3.Controls.Add(label8);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(label6);
+            groupBox3.Controls.Add(button4);
+            groupBox3.Controls.Add(richTextBox1);
+            groupBox3.Controls.Add(comboBox1);
+            groupBox3.Name = "groupBox3";
+            groupBox3.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(comboBox1, "comboBox1");
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Name = "comboBox1";
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(richTextBox1, "richTextBox1");
+            richTextBox1.Name = "richTextBox1";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(button4, "button4");
+            button4.BackColor = SystemColors.HotTrack;
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Name = "button4";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            resources.ApplyResources(label6, "label6");
+            label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(label7, "label7");
+            label7.Name = "label7";
+            label7.Click += label7_Click;
+            // 
+            // groupBox4
+            // 
+            resources.ApplyResources(groupBox4, "groupBox4");
+            groupBox4.Controls.Add(listView2);
+            groupBox4.Controls.Add(button5);
+            groupBox4.Controls.Add(label9);
+            groupBox4.Controls.Add(comboBox2);
+            groupBox4.Name = "groupBox4";
+            groupBox4.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            resources.ApplyResources(comboBox2, "comboBox2");
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Name = "comboBox2";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(label8, "label8");
+            label8.Name = "label8";
+            label8.Click += label8_Click;
+            // 
+            // label9
+            // 
+            resources.ApplyResources(label9, "label9");
+            label9.Name = "label9";
+            // 
+            // button5
+            // 
+            resources.ApplyResources(button5, "button5");
+            button5.BackColor = SystemColors.HotTrack;
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Name = "button5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // listView2
+            // 
+            resources.ApplyResources(listView2, "listView2");
+            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader3, columnHeader4, columnHeader5 });
+            listView2.Name = "listView2";
+            listView2.UseCompatibleStateImageBehavior = false;
+            listView2.View = View.Details;
+            // 
+            // columnHeader3
+            // 
+            resources.ApplyResources(columnHeader3, "columnHeader3");
+            // 
+            // columnHeader4
+            // 
+            resources.ApplyResources(columnHeader4, "columnHeader4");
+            // 
+            // columnHeader5
+            // 
+            resources.ApplyResources(columnHeader5, "columnHeader5");
+            // 
+            // groupBox5
+            // 
+            resources.ApplyResources(groupBox5, "groupBox5");
+            groupBox5.Controls.Add(richTextBox2);
+            groupBox5.Name = "groupBox5";
+            groupBox5.TabStop = false;
+            // 
+            // richTextBox2
+            // 
+            resources.ApplyResources(richTextBox2, "richTextBox2");
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.ReadOnly = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(tableLayoutPanel1, "tableLayoutPanel1");
+            tableLayoutPanel1.Controls.Add(groupBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(groupBox4, 1, 1);
+            tableLayoutPanel1.Controls.Add(groupBox3, 0, 1);
+            tableLayoutPanel1.Controls.Add(groupBox2, 1, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(groupBox5);
+            Name = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label1;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Label label5;
+        private TextBox textBox4;
+        private Button button3;
+        private Button button2;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private GroupBox groupBox3;
+        private Label label7;
+        private Label label6;
+        private Button button4;
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox1;
+        private Label label8;
+        private GroupBox groupBox4;
+        private Label label9;
+        private ComboBox comboBox2;
+        private ListView listView2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private Button button5;
+        private GroupBox groupBox5;
+        private RichTextBox richTextBox2;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
--- MessageQueueSystem/TestApplication/Form1.cs
+++ MessageQueueSystem/TestApplication/Form1.cs
@@ -6,5 +6,45 @@ namespace TestApplication
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
